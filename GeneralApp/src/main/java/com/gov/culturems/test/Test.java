f91db47cf548c3d89c43ac6d45368009fcfd1c32
yizhi401 <yizhi401@126.com>
2017-03-01 21:26:21 +0800

    all

--- GeneralApp/src/main/java/com/gov/culturems/test/Test.java
+++ GeneralApp/src/main/java/com/gov/culturems/test/Test.java
@@ -1,11 +1,10 @@
 package com.gov.culturems.test;
 
+import com.gov.culturems.entities.BaseSensor;
 import com.gov.culturems.utils.GsonUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by peter on 4/7/16.
@@ -17,24 +16,66 @@ public class Test {
         int ThresholdDown;
     }
 
-    public static void main(String[] args) {
-        Map<String, Object> paramsMap = new HashMap<String, Object>();
-//        paramsMap.putAll(HttpHeader.getInstance(context).getHeaderMap());
-        paramsMap.put("akey", "avalue");
-        List<UploadParam> list = new ArrayList<>();
-        UploadParam temp = new UploadParam();
-        temp.SensorType = "sensor1";
-        temp.ThresholdDown = 10;
-        temp.ThresholdUp = 11;
-        list.add(temp);
-        temp = new UploadParam();
-        temp.SensorType = "sensor2";
-        temp.ThresholdDown = 10;
-        temp.ThresholdUp = 11;
-        list.add(temp);
-        paramsMap.put("rule", list);
-        String body = GsonUtils.toJson(paramsMap);
+    static class AlarmReq {
+        String SensorType;
+        String ThresholdUp;
+        String ThresholdDown;
+    }
 
-        System.out.println(body);
+    static class WebsocketRequest {
+        String mt;
+        String ai;
+        String gi;
+        String di;
+        List<Rule> rules;
+        List<AlarmReq> alarms;
+    }
+
+    static class WebsocketResponse {
+        String ai;
+        String iscmded;
+    }
+
+    static class Rule {
+        String type;
+        String upper;
+        String lower;
+
+    }
+
+    public static void main(String[] args) {
+        WebsocketRequest newMessage = new WebsocketRequest();
+        newMessage.gi = "mygi";
+        newMessage.di = "mydi";
+        newMessage.mt = "mymt";
+//        newMessage.ai = AndroidUtil.getMyUUID(this);
+        newMessage.ai = "user id";
+        Rule temp = new Rule();
+        temp.lower = "30";
+        temp.upper = "30";
+        temp.type = BaseSensor.SENSOR_TEMPERATURE;
+        Rule humi = new Rule();
+        humi.lower = "30";
+        humi.upper = "30";
+        humi.type = BaseSensor.SENSOR_HUMIDITY;
+        newMessage.rules = new ArrayList<>();
+        newMessage.rules.add(temp);
+        newMessage.rules.add(humi);
+
+        newMessage.alarms = new ArrayList<>();
+        AlarmReq alarmReq = new AlarmReq();
+        alarmReq.SensorType = BaseSensor.SENSOR_TEMPERATURE;
+        alarmReq.ThresholdUp = "30";
+        alarmReq.ThresholdDown = "30";
+        newMessage.alarms.add(alarmReq);
+        alarmReq = new AlarmReq();
+        alarmReq.SensorType = BaseSensor.SENSOR_HUMIDITY;
+        alarmReq.ThresholdUp = "30";
+        alarmReq.ThresholdDown = "30";
+        newMessage.alarms.add(alarmReq);
+
+        String sentMessage = GsonUtils.toJson(newMessage);
+
+        System.out.println(sentMessage);
     }
 }
